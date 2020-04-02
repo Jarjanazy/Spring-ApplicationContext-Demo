60b7a57f6871aa8d0a1c9f5a5536436e49582a97
Jarjanazy <celil.cercenazi@gmail.com>
2020-04-02 16:32:24 +0300

    Add a separate config class and import it

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,20 +1,20 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(GreetingConfig.class) // this import tells Spring to look in GreetingConfig for Beans
 public class AppConfig {
-    @Bean String TestBean(){
-        return "Hello Test";
-    }
-
+    // an entry point
     public static void main(String[] args){
         // create a new Spring Context using the provided config class
         // the AnnotationConfigApplicationContext is one implementation of many
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        // get the bean called TestBean
-        String beanString = (String) applicationContext.getBean("TestBean");
+        // get the bean called helloGreeting
+        String beanString = (String) applicationContext.getBean("helloGreeting");
 
         System.out.println(beanString);
     }
